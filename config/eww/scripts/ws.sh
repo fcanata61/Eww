8c9e85d529146429d22f3f49405087f0ddfb91b9
fcanata61 <fernandocanatta26@gmail.com>
2025-08-31 20:17:00 -0300

    Update ws.sh

--- config/eww/scripts/ws.sh
+++ config/eww/scripts/ws.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-i3-msg -t get_workspaces | jq -r '.[] | select(.focused==true).name'
+# lista workspaces e marca ativo
+ACTIVE=$(wmctrl -d | awk '$2=="*" {print $1}')
+TOTAL=$(wmctrl -d | wc -l)
+
+OUT="["
+for i in $(seq 0 $((TOTAL-1))); do
+  if [ "$i" -eq "$ACTIVE" ]; then
+    OUT="$OUT \"1\""
+  else
+    OUT="$OUT \"0\""
+  fi
+done
+OUT="$OUT ]"
+echo $OUT
