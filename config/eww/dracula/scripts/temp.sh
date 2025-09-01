c63971ba4babf2ba30c3adf4261ed70c91ca30a3
fcanata61 <fernandocanatta26@gmail.com>
2025-08-31 21:42:08 -0300

    Update temp.sh

--- config/eww/dracula/scripts/temp.sh
+++ config/eww/dracula/scripts/temp.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sensors | awk '/^Package id 0:/ {gsub("+",""); print int($4)}' || echo 0
+# Retorna a temperatura da CPU em %
+# Ajuste o hwmon correto se necessário
+sensors | grep -m 1 'Tctl:' | awk '{gsub("+",""); gsub(".0°C",""); print int($2)}'
