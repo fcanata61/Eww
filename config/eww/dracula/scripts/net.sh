409babf61b8fce27d60f1b5d5d2101c489e2c58d
fcanata61 <fernandocanatta26@gmail.com>
2025-08-31 21:42:49 -0300

    Update net.sh

--- config/eww/dracula/scripts/net.sh
+++ config/eww/dracula/scripts/net.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-RX1=$(cat /sys/class/net/*/statistics/rx_bytes | awk '{s+=$1} END {print s}')
+# Mostra velocidade de download/upload em KB/s
+# Ajuste sua interface de rede (ex: eth0, enp3s0, wlan0)
+
+INTERFACE="wlan0"
+RX1=$(cat /sys/class/net/$INTERFACE/statistics/rx_bytes)
+TX1=$(cat /sys/class/net/$INTERFACE/statistics/tx_bytes)
 sleep 1
-RX2=$(cat /sys/class/net/*/statistics/rx_bytes | awk '{s+=$1} END {print s}')
-DOWN=$(( (RX2 - RX1) / 1024 ))
-VAL=$(( DOWN > 100 ? 100 : DOWN ))
-echo $VAL
+RX2=$(cat /sys/class/net/$INTERFACE/statistics/rx_bytes)
+TX2=$(cat /sys/class/net/$INTERFACE/statistics/tx_bytes)
+
+RX_RATE=$(( (RX2 - RX1) / 1024 ))
+TX_RATE=$(( (TX2 - TX1) / 1024 ))
+
+echo "$((RX_RATE+TX_RATE))"
